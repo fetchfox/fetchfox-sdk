0d8dce1b1cda2eef4fae57dc8c20551300d0bc38
ortutay <marcell.ortutay@gmail.com>
2025-07-30 15:14:54 -0700

    detach scrape

--- src/crawl.js
+++ src/crawl.js
@@ -4,6 +4,7 @@ import { Job } from './detach.js';
 export const crawl = async function () {
   return call('POST', '/api/crawl', args);
 };
+
 crawl.detach = async (args) => {
   const data = await call('POST', '/api/crawl', { ...args, detach: true });
   console.log('detach data', data);
--- src/scrape.js
+++ src/scrape.js
@@ -1,5 +1,12 @@
 import { call } from './api.js';
+import { Job } from './detach.js';
 
 export const scrape = async (args) => {
   return call('POST', '/api/scrape', args);
+};
+
+scrape.detach = async (args) => {
+  const data = await call('POST', '/api/scrape', { ...args, detach: true });
+  console.log('detach data', data);
+  return new Job(data.jobId);
 };
